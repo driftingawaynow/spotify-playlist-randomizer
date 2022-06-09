6ceb48fe9b550321f70a01cb837944558fa6acb0
Chaz630 <36994814+driftingawaynow@users.noreply.github.com>
2022-06-08 21:04:34 -0400

    stuff

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const querystring = require('querystring');
 const app = express();
-const port = 8888;
+const PORT = process.env.PORT || 8888;
 const axios = require('axios');
+const path = require('path');
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
+const FRONTEND_URI = process.env.FRONTEND_URI;
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
 /**
  * Generates a random string containing numbers and letters
@@ -78,7 +83,7 @@ app.get('/callback', (req, res) => {
               expires_in
           });
             // redirect to react app
-            res.redirect(`http://localhost:3000/?${queryParams}`);
+            res.redirect(`${FRONTEND_URI}/?${queryParams}`);
             // pass along tokens to query params
     
         } else {
@@ -120,6 +125,11 @@ app.get('/generate', (req, res) => {
   })
 });
 
-app.listen(port, () => {
-    console.log(`Express app listening at http://localhost:${port}`);
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Express app listening at http://localhost:${PORT}`);
 });
